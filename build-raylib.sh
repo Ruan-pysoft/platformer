04621198459f1c1f39bc911af752bf5d97b80aca
Ruan <ruan@pysoft.co.za>
2025-10-01 23:24:33 +0200

    Add raylib build to nob

--- build-raylib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x -e
-
-cd raylib/src
-
-make PLATFORM=PLATFORM_DESKTOP RAYLIB_BUILD_MODE=DEBUG
-
-cd ../..
--- src_build/nob_configed.c
+++ src_build/nob_configed.c
@@ -6,18 +6,67 @@
 #define NOB_WARN_DEPRECATED
 #include "nob.h"
 
+bool build_raylib(void);
+bool build_game(void);
+
+int main(int argc, char **argv) {
+	if (!build_raylib()) return 1;
+	if (!build_game()) return 1;
+}
+
 #include "dirs.h"
 #include "config.h"
 
+// add the config.h header as a dependency to the executables,
+// as changing the config can affect the compiler used as well as the flags
+const char config_header[] = BUILD_DIR"config.h";
+
+/* BUILDING THE RAYLIB LIBRARY */
+
+bool build_raylib(void) {
+#ifdef WINDOWS
+	nob_log(WARNING, "Cross-building for windows, raylib isn't built locally. Instead download the %s raylib release from https://github.com/raysan5/raylib", "raylib-5.5_win64_mingw-w64");
+#else
+	const char *libraylib = "raylib/src/libraylib.a";
+
+	// debug raylib and release raylib are different
+	bool raylib_needs_rebuild = false;
+	if (!raylib_needs_rebuild) {
+		int exists = file_exists(libraylib);
+		if (exists < 0) return false;
+		raylib_needs_rebuild = exists == 0;
+	}
+	if (!raylib_needs_rebuild) {
+		raylib_needs_rebuild = needs_rebuild1(libraylib, config_header);
+	}
+	if (!raylib_needs_rebuild) return true;
+
+	if (chdir("raylib/src") == -1) return true;
+
+	Cmd cmd = {0};
+
+	cmd_append(&cmd, "make", "PLATFORM=PLATFORM_DESKTOP");
+#ifndef RELEASE
+	cmd_append(&cmd, "RAYLIB_BUILD_MODE=DEBUG");
+#endif
+
+	if (chdir("../..") == -1) return true;
+#endif
+
+	return true;
+}
+
+/* BUILDING THE GAME EXECUTABLE */
+
+// final executable differs between windows cross-build and native build
 #ifdef WINDOWS
 const char outfile[] = BUILD_DIR"game.exe";
 #else
 const char outfile[] = BUILD_DIR"game";
 #endif
-const char config_header[] = BUILD_DIR"config.h";
 
+// list headers used in the project
 #define HPP(header) const char *const header ## _hpp = INCLUDE_DIR #header ".hpp"
-
 HPP(actions);
 HPP(game);
 HPP(gui);
@@ -30,45 +79,36 @@ HPP(main_menu);
 HPP(player);
 HPP(scene);
 
-#define DEPS(of) const char *const of ## _deps[] =
-#define NO_DEPS(of) const char *const of ## _deps[] = { of ## _hpp }
+// list the headers each .cpp file depends on
+#define HEADERS(of, ...) const char *const of ## _headers[] = { of ## _hpp, __VA_ARGS__ }
+#define HEADERS_NO_SELF(of, ...) const char *const of ## _headers[] = { __VA_ARGS__ }
 
-DEPS(main) {
-	actions_hpp, input_manager_hpp, game_hpp,
-	globals_hpp,
-};
-DEPS(game) {
-	game_hpp,
-	globals_hpp, main_menu_hpp, scene_hpp,
-};
-DEPS(player) { player_hpp, level_hpp };
-NO_DEPS(input_manager);
-DEPS(actions) { actions_hpp, input_manager_hpp };
-DEPS(level) {
-	level_hpp,
-	actions_hpp, globals_hpp, levels_list_hpp,
-	main_menu_hpp, player_hpp, scene_hpp,
-};
-DEPS(main_menu) {
-	main_menu_hpp,
-	globals_hpp, level_select_hpp, levels_list_hpp,
-};
-NO_DEPS(levels_list);
-DEPS(gui) { gui_hpp };
-DEPS(level_select) {
-	level_select_hpp,
-	levels_list_hpp, main_menu_hpp,
-};
+HEADERS_NO_SELF(main, actions_hpp, input_manager_hpp, game_hpp, globals_hpp);
+HEADERS(game, globals_hpp, main_menu_hpp, scene_hpp);
+HEADERS(player, level_hpp);
+HEADERS(input_manager);
+HEADERS(actions, input_manager_hpp);
+HEADERS(level,
+	actions_hpp, globals_hpp, levels_list_hpp, main_menu_hpp, player_hpp,
+	scene_hpp
+);
+HEADERS(main_menu, globals_hpp, level_select_hpp, levels_list_hpp);
+HEADERS(levels_list);
+HEADERS(gui);
+HEADERS(level_select, levels_list_hpp, main_menu_hpp);
 
+// most files in the project exists in src/name.cpp, outputs to build/name.o,
+// and depends on the headers defined in name_headers
 #define STANDARD_FILE(name) \
 	{ SRC_DIR #name ".cpp", BUILD_DIR #name ".o", \
-	  name ## _deps, ARRAY_LEN(name ## _deps) }
+	  name ## _headers, ARRAY_LEN(name ## _headers) }
 
+// list the files in the project
 struct Target {
 	const char *src;
 	const char *obj;
-	const char *const *deps;
-	size_t n_deps;
+	const char *const *headers;
+	size_t n_headers;
 } targets[] = {
 	STANDARD_FILE(main),
 	STANDARD_FILE(game),
@@ -82,20 +122,26 @@ struct Target {
 	STANDARD_FILE(level_select),
 };
 
-bool check_needs_rebuild(const struct Target *target) {
+// check if a particular file needs rebuilding
+bool target_needs_rebuild(const struct Target *target) {
+	// if the config header has changed, it needs to be rebuilt
 	if (needs_rebuild1(target->obj, config_header)) return true;
+	// if the file itself has changed, it needs to be rebuilt
 	if (needs_rebuild1(target->obj, target->src)) return true;
+	// if any of its headers have changed, it needs to be rebuilt
 	return needs_rebuild(
-		target->obj, (const char**)target->deps, target->n_deps
+		target->obj, (const char**)target->headers, target->n_headers
 	);
 }
 
+// select a cross-compiler if required in config.h
 #ifdef WINDOWS
 const char *compiler = "x86_64-w64-mingw32-g++";
 #else
 const char *compiler = "g++";
 #endif
 
+// flags used for compiling a certain c++ source to a .o file
 const char *cpp_flags[] = {
 #ifdef RELEASE
 	"-O2",
@@ -105,11 +151,22 @@ const char *cpp_flags[] = {
 	"-Wall", "-Wextra",
 	"-I./include", "-I./raylib/src",
 };
+// flags used for compiling all the .o files into the final executable
 const char *ld_flags[] = {
 	"-O2",
 #ifdef WINDOWS
 	"-L./raylib-5.5_win64_mingw-w64/lib",
+	// idk why, but we need to link extra stuff for it to work on Windows
+	// (at least if you plan on running the executable through wine)
 	"-lraylib", "-lm", "-lgdi32", "-lwinmm",
+	// link it statically, easier than making sure that the end-user has
+	// all the libc and libc++ dlls
+	// NOTE: this might complicate distribution, as I believe they have
+	// the GNU GPL license? So I can't legally share the compiled windows
+	// .exe around without the license text and some way for the user to
+	// access the source code of the statically linked libraries.
+	// A url to whichever git repository my cross-compiler comes from
+	// should be enough?
 	"-static",
 #else
 	"-L./raylib/src",
@@ -117,48 +174,72 @@ const char *ld_flags[] = {
 #endif
 };
 
+// utility to add an array of strings to a cmd; I believe one exists for adding
+// a cmd to another one in nob.h, but for some reason not this one? I might
+// just be missing it
 void cmd_push(Cmd *cmd, const char **args, size_t args_len) {
 	for (size_t i = 0; i < args_len; ++i) {
 		cmd_append(cmd, args[i]);
 	}
 }
 
-int main(int argc, char **argv) {
+bool build_game(void) {
 	Cmd cmd = {0};
 	Procs procs = {0};
 
+	// keep track of if the executable actually needs to be rebuilt; if
+	// config.h has changed then it does...
+	bool exe_needs_rebuild = needs_rebuild1(outfile, config_header);
+
 	for (size_t i = 0; i < ARRAY_LEN(targets); ++i) {
-		if (!check_needs_rebuild(&targets[i])) continue;
+		// don't rebuild it if it doesn't need to be rebuilt
+		// (this saves a *lot* of dev time)
+		if (!target_needs_rebuild(&targets[i])) continue;
+
+		// ...and if any .o file was rebuilt, the executable also needs
+		// to be rebuilt
+		exe_needs_rebuild = true;
 
 		cmd_append(&cmd, compiler, "-c");
 		cmd_push(&cmd, cpp_flags, ARRAY_LEN(cpp_flags));
 		cmd_append(&cmd, "-o", targets[i].obj);
 		cmd_append(&cmd, targets[i].src);
 
-		if (!cmd_run(&cmd, .async = &procs)) return 1;
+		// and build asynchronously, by default the number of threads
+		// is the number of processors on the machine (which might be
+		// threads or might be cores, depending on OS, iirc)
+		// once again, this saves a *lot* of dev time
+		// (I think my laptop has four "processors", which would
+		// roughly speed up build times 4x)
+		if (!cmd_run(&cmd, .async = &procs)) return false;
 	}
 
-	if (!procs_flush(&procs)) return 1;
+	// wait for all .o files to build before linking them together
+	if (!procs_flush(&procs)) return false;
 
-	bool needs_rebuild = needs_rebuild1(outfile, config_header);
+	// if neither config.h has changed, nor any .o files were rebuilt, then
+	// we don't need to rebuild the executable
+	if (exe_needs_rebuild) {
+		cmd_append(&cmd, compiler);
+		cmd_append(&cmd, "-o", outfile);
+		for (size_t i = 0; i < ARRAY_LEN(targets); ++i) {
+			cmd_append(&cmd, targets[i].obj);
+		}
+		// note that (iirc) linker flags like "-lm" should go *after*
+		// all the objects, for some reason?
+		cmd_push(&cmd, ld_flags, ARRAY_LEN(ld_flags));
 
-	cmd_append(&cmd, compiler);
-	cmd_append(&cmd, "-o", outfile);
-	for (size_t i = 0; i < ARRAY_LEN(targets); ++i) {
-		cmd_append(&cmd, targets[i].obj);
-
-		if (nob_needs_rebuild1(outfile, targets[i].obj)) needs_rebuild = true;
-	}
-	for (size_t i = 0; i < ARRAY_LEN(ld_flags); ++i) {
-		cmd_append(&cmd, ld_flags[i]);
-	}
-
-	if (needs_rebuild) {
-		if (!cmd_run(&cmd)) return 1;
+		if (!cmd_run(&cmd)) return false;
 
 #ifdef RELEASE
+		// and strip the exe on release builds, results in a smaller
+		// exe (and theoretically harder to reverse-engineer, but I
+		// don't really care about that, I plan on having this
+		// open-source anyways)
 		cmd_append(&cmd, "strip", outfile);
-		if (!cmd_run(&cmd)) return 1;
+		if (!cmd_run(&cmd)) return false;
 #endif
 	} else nob_log(INFO, "Game executable is up-to-date, not rebuilding!");
+
+	return true;
 }
